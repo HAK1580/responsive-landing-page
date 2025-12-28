4b96d6ebec6feb7be90fa8f49eebf45380c5a5e2
HAK1580 <aliofficial1580630@gmail.com>
2025-12-28 17:09:58 +0500

    good at it

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 const Hamburger = () => {
   return (
-    <div className='border mt-[-0.3rem] bg-gray-200 fixed right-0 mx-auto h-full p-1 w-[80%] ' >
-          <ul className='flex flex-col ml-6 mt-5 gap-6'>
-            <li><a className='text-[1.2rem] ham-a' href="/">Home</a></li>
-            <li><a className='text-[1.2rem] ham-a' href="/about">About</a></li>
-            <li><a className='text-[1.2rem] ham-a' href="/skills">Features</a></li>
-            <li><a className='text-[1.2rem] ham-a' href="/projects">Contacts</a></li>
+    <div className='border border-gray-500 mt-[-0.25rem] p-1  bg-gray-200 fixed right-0 mx-auto h-full  w-[75%] ' >
+          <ul className='flex flex-col ml-5 mt-5 gap-6'>
+            <li><a className='text-[1.15rem] ham-a' href="/">Home</a></li>
+            <li><a className='text-[1.15rem] ham-a' href="/about">About</a></li>
+            <li><a className='text-[1.15rem] ham-a' href="/skills">Features</a></li>
+            <li><a className='text-[1.15rem] ham-a' href="/projects">Contacts</a></li>
         </ul>
          <div className="social-media mt-18 m-6">
-            <h1 className='uppercase   tracking-wide  soge text-[1.4rem]'>Social </h1>
-           <div className="icons py-12 gap-5 flex justify-center">
+            <h1 className='uppercase   tracking-wide  soge text-[1.3rem]'>Social </h1>
+           <div className="icons py-12 gap-4 flex justify-center">
            <a target='_blank' href="https://github.com/HAK1580"> <img  className='w-10' src="github.svg" alt="" /> </a>
            <a target='_blank' href="https://www.tiktok.com/@codewithme._.01"> <img className='w-10' src="tiktok.svg" alt="" />  </a>
            <a target='_blank' href="https://www.linkedin.com/in/muhammad-hassan-ali-khan-49a965281/ "> <img className='w-10' src="linkedin.svg" alt="" /> </a>
